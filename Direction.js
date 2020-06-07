537b44672c4496e704ab3b3a8d179c7ed4e25aaf
Tristan Menzinger <t.menzinger@gmail.com>
2020-06-07 16:44:28 +0200

    cleaned up

--- Direction.js
+++
@@ -1,9 +0,0 @@
-const Direction = {
-  DOWN: Symbol('DOWN'),
-  INVALID: Symbol('INVALID'),
-  LEFT: Symbol('LEFT'),
-  RIGHT: Symbol('RIGHT'),
-  UP: Symbol('UP'),
-};
-
-export default Direction;
--- index.js
+++
@@ -1,9 +0,0 @@
-import Card from './Card';
-import Direction from './Direction';
-import Stack from './Stack';
-
-export {
-  Card,
-  Direction,
-  Stack,
-};
